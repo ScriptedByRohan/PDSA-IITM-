ed018ec4e33cac20bece72de4244e2d685ea2c2a
Rohan singh negi <negirohan10ep@gmail.com>
2026-08-05 22:37:40 +0530

    Add files via upload

--- Week-3/grpa3.py
+++ Week-3/grpa3.py
@@ -4,8 +4,8 @@ def reverse(root):
     prev = None
     current = root
     while current is not None:
-        nextnode = current.next
-        current.next = prev
-        prev = current
-        current = nextnode
+        nextnode = current.next # save the next node . so while pointing first node to the node we does'n loose the pointer that point the other nodes
+        current.next = prev # Simply it is used to reverse the arrow . It points the first node to the none
+        prev = current # Move previous
+        current = nextnode # Move current 
     return prev
